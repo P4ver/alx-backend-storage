0dc52406178c1608e809d7d8820ca91e74d4aeae
P4ver <s.pales20@gmail.com>
2024-06-19 10:56:43 -0700

    es

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,31 +1,45 @@
 #!/usr/bin/env python3
-""" Log stats,"""
-
+""" 12. Log stats
+"""
 
 from pymongo import MongoClient
 
 
-def display_log_stats():
-    """ display_log_stats,"""
-    mongo_client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_logs = mongo_client.logs.nginx
-    total_logs = nginx_logs.count_documents({})
-    get_requests = nginx_logs.count_documents({"method": "GET"})
-    post_requests = nginx_logs.count_documents({"method": "POST"})
-    put_requests = nginx_logs.count_documents({"method": "PUT"})
-    patch_requests = nginx_logs.count_documents({"method": "PATCH"})
-    delete_requests = nginx_logs.count_documents({"method": "DELETE"})
-    status_checks = nginx_logs.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{total_logs} logs")
-    print("Methods:")
-    print(f"\tmethod GET: {get_requests}")
-    print(f"\tmethod POST: {post_requests}")
-    print(f"\tmethod PUT: {put_requests}")
-    print(f"\tmethod PATCH: {patch_requests}")
-    print(f"\tmethod DELETE: {delete_requests}")
-    print(f"{status_checks} status check")
+def log_stats():
+    """ log_stats.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    
+    total = logs_collection.count_documents({})
+    
+    if total == 0:
+        print(f"{total} logs")
+        print("Methods:")
+        print(f"\tmethod GET: 0")
+        print(f"\tmethod POST: 0")
+        print(f"\tmethod PUT: 0")
+        print(f"\tmethod PATCH: 0")
+        print(f"\tmethod DELETE: 0")
+        print(f"0 status check")
+        return
 
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
 
 if __name__ == "__main__":
-    display_log_stats()
+    log_stats()
